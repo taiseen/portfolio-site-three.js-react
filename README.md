44b752a02d5456f8a17a0407c85bcdf490973fd7
taiseen <taiseen.cse@gmail.com>
2023-03-02 20:39:59 +0600

    feat: project init

---
+++ README.md
@@ -0,0 +1,8 @@
+> 02 - March - 2023
+
+# Portfolio Site
+
+##Technology used:
+* React
+* Three.js
+* Tailwind-CSS
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+ 
+
+  return (
+    <div className="container mx-auto bg-red-500">
+      hello
+    </div>
+  )
+}
+
+export default App
